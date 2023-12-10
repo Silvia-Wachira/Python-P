801f79f67a23c566525371735dc5585d238a028e
Silvia-Wachira <slywachira96@gmail.com>
2023-12-10 18:30:42 +0300

    create a arithmetic.py file

---
+++ arithmetic2.py
@@ -0,0 +1,8 @@
+# Arithmetic with strings
+
+a = "a"
+b = "b"
+an = "an"
+
+print(b + an)
+print(b + a*7)
--- input.py
+++ input.py
@@ -1,1 +1,10 @@
-name = input("What is your name?")
+# name = input("What is your name?\n")
+# print(name)
+
+# print("Hi, " + name + ".")
+# print("Hi, %s." % name)
+# print("Hi, {0}.".format(name))
+#
+
+d = datetime.datetime(2020, 7, 4, 12, 15, 58)
+print('{:%Y-%m-%d %H:%M:%S}'.format(d))
