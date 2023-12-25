ef25e661be50ac834c1e643a52032f883d8be50c
Silvia-Wachira <slywachira96@gmail.com>
2023-12-25 07:50:57 +0300

    0

--- arithmetic2.py
+++ arithmetic2.py
@@ -1,8 +1,17 @@
 # Arithmetic with strings
 
 x = 25
-y = 17.1
+y = 17.0
 
-print(x)
-print(y)
+# print(x)
+# print(y)
 
+# print(x + y)
+# print( x - y)
+# print(x * y)
+# print( x / y)
+# print( x ** y)
+# print(x // y)
+# print( x % y)
+
+print(f'The result is {int(x // y)} remainder {int(x % y)}')
