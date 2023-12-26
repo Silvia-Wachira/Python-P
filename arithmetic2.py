b545c27557da7ed7e7160c9b875bbf4c02484d98
Silvia-Wachira <slywachira96@gmail.com>
2023-12-26 21:44:34 +0300

    perform arithmetic

--- arithmetic2.py
+++ arithmetic2.py
@@ -1,7 +1,7 @@
 # Arithmetic with strings
 
-x = 25
-y = 17.0
+# x = 25
+# y = 17.0
 
 # print(x)
 # print(y)
@@ -14,4 +14,13 @@ y = 17.0
 # print(x // y)
 # print( x % y)
 
-print(f'The result is {int(x // y)} remainder {int(x % y)}')
+# print(f'The result is {int(x // y)} remainder {int(x % y)}')
+
+# print(x ** 2)
+# print(y ** 2)
+
+x = 25
+y = 17.6
+
+print(x * y)
+
