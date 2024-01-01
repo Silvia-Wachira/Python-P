7c841c039532047388ebc17831a0c5f824ec0f28
Silvia-Wachira <slywachira96@gmail.com>
2024-01-02 00:22:47 +0300

    typecast

--- identity.py
+++ identity.py
@@ -1,3 +1,18 @@
 a = 1
 b = 1.0
 c = "1"
+
+# print(a == b)
+# print(a is b)
+
+# print(c == "1")
+# print(c is "1")
+
+# print(b == 1)
+# print(b is 1)
+
+print(b == 1 and isinstance(b, int))
+print(a == 1 and isinstance(a, int))
+
+b = int(b)
+print(b)
