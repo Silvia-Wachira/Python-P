b2e151df795396d8da479d157916e398eac5829c
Silvia-Wachira <slywachira96@gmail.com>
2024-01-03 19:33:10 +0300

    loop in js

--- identity.py
+++ identity.py
@@ -11,8 +11,18 @@ c = "1"
 # print(b == 1)
 # print(b is 1)
 
-print(b == 1 and isinstance(b, int))
-print(a == 1 and isinstance(a, int))
+# print(b == 1 and isinstance(b, int))
+# print(a == 1 and isinstance(a, int))
 
-b = int(b)
-print(b)
+# b = int(b)
+# print(b)
+# print(b == 1 and isinstance(b, int))
+
+# print(a)
+# print(float(a))
+# print(str(a))
+
+print('My name is ')
+for i in range(5):
+    print('Carlita Cinco (' + str(i) +')')
+
