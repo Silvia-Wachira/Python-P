5e1b6599019300e8a6e242dff7d094f41e9d01f9
Silvia-Wachira <slywachira96@gmail.com>
2024-01-20 00:09:49 +0300

    0

--- equality.py
+++ equality.py
@@ -3,19 +3,19 @@
 # c = "1"
 
 
-# print(a == b)
-# print(a == c)
-# print(b == c)
+# # print(a == b)
+# # print(a == c)
+# # print(b == c)
 
-if (a == c):
-    print("match")
-elif (a == b):
-    print("a matches b")
-else:
-    print("not a match")
+# if (a == c):
+#     print("match")
+# elif (a == b):
+#     print("a matches b")
+# else:
+#     print("not a match")
 
-a = ['a', 'b', 'c']
-if (a):
-    print(f'{a} is true') 
-else: 
-    print(f'{a} is false')
+# a = ['a', 'b', 'c']
+# if (a):
+#     print(f'{a} is true') 
+# else: 
+#     print(f'{a} is false')
