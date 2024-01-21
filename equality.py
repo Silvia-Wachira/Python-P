864300f6d988ce12b7fbc78fce04e52a7d9b3c64
Silvia-Wachira <slywachira96@gmail.com>
2024-01-21 19:21:26 +0300

    0

--- equality.py
+++ equality.py
@@ -1,21 +1,21 @@
-# a = 1
-# b = 1.0
-# c = "1"
+a = 1
+b = 1.0
+c = "1"
 
 
-# # print(a == b)
-# # print(a == c)
-# # print(b == c)
+# print(a == b)
+# print(a == c)
+# print(b == c)
 
-# if (a == c):
-#     print("match")
-# elif (a == b):
-#     print("a matches b")
-# else:
-#     print("not a match")
+if (a == c):
+    print("match")
+elif (a == b):
+    print("a matches b")
+else:
+    print("not a match")
 
-# a = ['a', 'b', 'c']
-# if (a):
-#     print(f'{a} is true') 
-# else: 
-#     print(f'{a} is false')
+a = ['a', 'b', 'c']
+if (a):
+    print(f'{a} is true') 
+else: 
+    print(f'{a} is false')
