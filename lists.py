a43dee7ca7729719c264b26015d3dcfe52cf0c91
Silvia-Wachira <slywachira96@gmail.com>
2024-02-03 01:07:43 +0300

    create a tuples file

--- lists.py
+++ lists.py
@@ -1,5 +1,32 @@
-empty = []
-print(empty)
+# supplies = ['crayons', 'pencils', 'paper', 'Kleenex', 'eraser']
+# print(supplies)
 
-friends = ["Sammy", "Jo", "Will", "Sara"]
-print(friends)
+# supplies.append('markers')
+# print(supplies)
+
+# supplies.remove('crayons')
+# print(supplies)
+
+# supplies.sort()
+# print(supplies)
+
+# supplies.sort(key=str.lower)
+# print(supplies)
+
+colors = ['red', 'orange', 'blue', 'pink']
+
+alphabetical = sorted(colors)
+print(colors)
+print(alphabetical)
+
+alphabetical = sorted(colors,reverse=True)
+print(alphabetical)
+
+reversedColors = reversed(colors)
+reversedAlpha = reversed(alphabetical)
+
+print(reversedColors)
+print(reversedAlpha)
+
+print(list(reversedColors))
+print(list(reversedAlpha))
