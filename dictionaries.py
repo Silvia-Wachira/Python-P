38e8c44ee68861901da67d4b6cb04dcd2342f119
Silvia-Wachira <slywachira96@gmail.com>
2024-02-06 22:50:05 +0300

    create a sets.py file

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,35 @@
 
+# pond = dict(
+# depth= 10,
+# area='210 square feet',
+# fish=['Mary', 'Bob', 'Billy']
+# )
 
-book = {
-    'title': 'Goodnight Moon',
-    'ratings':7492, 
-    'stars': 4.0,
-    'author': {'firstMame', }
-}
+# # print(pond)
 
-print(book)
+# alligator = dict([
+#     ('lifespan', 54),
+#     ('length', 3.6),
+#     ('lifespan', 54),
+#     ('species', ['American alligator, Chinese alligator']),
+#     ('funfact', "As an alligator's teeth are down, they are replaced."
+#                 + "An alligator can go through 3,000 teeth in a life time"),
+# ])
+# print(alligator)
+
+
+# keys = ['name', 'home-runs', 'strikeout', 'rbi']
+# values = ['Babe Ruth', '7214', 1330, 2214]
+# player = dict(zip(keys, values))
+# print(dir(player))
+
+spam = {'color': 'red', 'age': 42}
+for v in spam.values ():
+    print(v)
+
+for k in spam.keys():
+    print(k)
+
+
+for k, v in spam.items():
+    print('Key: ' + k + ' Value: ' + str(v))
---
+++ sets.py
@@ -0,0 +1,30 @@
+# # a = {1, 2, 3}
+# # b = {3, 4, 5}
+
+# # print(a)
+# # print(b)
+
+# # print(a | b)
+# # print(a & b)
+
+# # print(a - b)
+# # print(b - a)
+# # print(a^b)
+# # print(a + b)
+
+# a = set('banana')
+# b = set('scarab')
+
+# print(a)
+# print(b)
+# print(a | b)
+# print(a.symmetric_difference(b))
+
+# print(a.union(b))
+# print(a.intersection(b))
+# print(a.difference(b))
+# print(b.difference(a))
+
+basket = ['apple', 'banana', 'apple', 'orange','pear', 'apple']
+print(basket)
+print(set(basket))
